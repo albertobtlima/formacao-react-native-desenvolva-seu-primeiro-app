a3d864cd7002dba2c7a85b1d18a33be8eb793f50
Alberto Lima <alberto.btlima@outlook.com>
2025-11-04 15:44:20 -0300

    add: button comecar e footer

--- 01-criacao-de-projeto-no-react-native/fokus/app/index.jsx
+++ 01-criacao-de-projeto-no-react-native/fokus/app/index.jsx
@@ -1,10 +1,21 @@
-import { Image, StyleSheet, View } from "react-native";
+import { Image, Pressable, StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <Image source={require("./pomodoro.png")} />
-      <View style={styles.actions} />
+      <View style={styles.actions}>
+        <Text style={styles.timer}>25:00</Text>
+        <Pressable style={styles.button}>
+          <Text style={styles.buttonText}>Começar</Text>
+        </Pressable>
+      </View>
+      <View style={styles.footer}>
+        <Text style={styles.footerText}>
+          Projeto fictício e sem fins comerciais.
+        </Text>
+        <Text style={styles.footerText}>Desenvolvido por Alura & Alberto.</Text>
+      </View>
     </View>
   );
 }
@@ -24,5 +35,30 @@ const styles = StyleSheet.create({
     borderRadius: 32,
     borderWidth: 2,
     borderColor: "#144480",
+    gap: 32,
+  },
+  timer: {
+    fontSize: 53,
+    fontWeight: "bold",
+    color: "#fff",
+    textAlign: "center",
+  },
+  button: {
+    backgroundColor: "#b872ff",
+    borderRadius: 32,
+    padding: 8,
+  },
+  buttonText: {
+    textAlign: "center",
+    color: "#021123",
+    fontSize: 18,
+  },
+  footer: {
+    width: "80%",
+  },
+  footerText: {
+    textAlign: "center",
+    color: "#98a0a8",
+    fontSize: 12.5,
   },
 });
