d28e888a17d58ce6d461520d68be9151f190d842
Alberto Lima <alberto.btlima@outlook.com>
2025-11-18 15:33:33 -0300

    add: icones de edição, checar e deletar

--- 02-navegando-entre-telas-com-expo-router/fokus2.0/app/_layout.jsx
+++ 02-navegando-entre-telas-com-expo-router/fokus2.0/app/_layout.jsx
@@ -4,7 +4,52 @@ import { GestureHandlerRootView } from "react-native-gesture-handler";
 export default function Layout() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <Drawer />
+      <Drawer
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "#021123",
+          },
+          headerTintColor: "#fff",
+          drawerStyle: {
+            backgroundColor: "#021123",
+          },
+          drawerLabelStyle: {
+            color: "#fff",
+          },
+        }}
+      >
+        <Drawer.Screen
+          name="index"
+          options={{
+            headerShown: false,
+            drawerItemStyle: {
+              display: "none",
+            },
+          }}
+        />
+        <Drawer.Screen
+          name="add-task/index"
+          options={{
+            drawerItemStyle: {
+              display: "none",
+            },
+          }}
+        />
+        <Drawer.Screen
+          name="pomodoro"
+          options={{
+            drawerLabel: "Timer",
+            title: "",
+          }}
+        />
+        <Drawer.Screen
+          name="tasks/index"
+          options={{
+            drawerLabel: "Lista de tarefas",
+            title: "",
+          }}
+        />
+      </Drawer>
     </GestureHandlerRootView>
   );
 }
---
+++ 02-navegando-entre-telas-com-expo-router/fokus2.0/components/TaskItem/index.jsx
@@ -0,0 +1,53 @@
+import { View, Text, Pressable, StyleSheet } from "react-native";
+import { IconCheck, IconPencil, IconTrash } from "../Icons";
+
+export const TaskItem = ({
+  completed,
+  text,
+  onToggleComplete,
+  onPressEdit,
+  onPressDelete,
+}) => {
+  const cardStyles = [styles.card];
+
+  if (completed) {
+    cardStyles.push(styles.carCompleted);
+  }
+
+  return (
+    <View style={cardStyles}>
+      <Pressable onPress={onToggleComplete}>
+        <IconCheck checked={completed} />
+      </Pressable>
+      <Text style={styles.text}>{text}</Text>
+      <Pressable onPress={onPressEdit}>
+        <IconPencil />
+      </Pressable>
+      <Pressable onPress={onPressDelete}>
+        <IconTrash />
+      </Pressable>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  card: {
+    flexDirection: "row",
+    backgroundColor: "#98a0a8",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingHorizontal: 8,
+    paddingVertical: 18,
+    borderRadius: 8,
+    gap: 8,
+  },
+  carCompleted: {
+    backgroundColor: "#0f725c",
+  },
+  text: {
+    flex: 1,
+    color: "#021123",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+});
